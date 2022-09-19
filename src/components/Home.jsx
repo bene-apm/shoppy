a3f390a4b3174b5b394d167f7599252548dc0fc1
ben <ben@bens-MacBook-Air.local>
2022-09-19 15:02:03 +0700

    up

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,9 +5,9 @@ const Home = () => {
     return (
         <div className='hero'>
             <div className="card bg-dark text-white border-0">
-                <img className="card-img" src="/assets/banner.jpg" alt="Banner" height='550px'/>
+                <img className="card-img" href="https://www.freepik.com/free-photo/stylish-asian-girl-shopper-wears-sunglasses-holding-shopping-bag-smartphone-going-discounts-stores-standing-white-background_25980263.htm#query=shopping%20white%20background&position=12&from_view=search" height='550px'/>
                     <div className="card-img-overlay d-flex flex-column justify-content-center">
-                        <div className="container">
+                        <div className="container bg">
                         <h5 className="card-title display-3 fw-bolder mb-0 text-warning">NEW ARRIVALS </h5>
                         <p className="card-text lead fs-2 text-warning">BEFORE ANYONE ELSE</p>
                         </div>
